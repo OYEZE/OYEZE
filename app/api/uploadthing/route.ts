823315fd42d58c8679cdeb86ca075fe061297021
OYEZE <z5348779@gmail.com>
2026-06-07 06:39:31 -0500

    Update route.ts

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,17 +1,10 @@
 import { createRouteHandler } from "uploadthing/next";
 
-const getHandlers = async () => {
-  if (!process.env.UPLOADTHING_TOKEN) {
-    return {
+const handlers = process.env.UPLOADTHING_TOKEN
+  ? createRouteHandler({ router: require("./core").ourFileRouter })
+  : {
       GET: () => new Response("Upload disabled", { status: 200 }),
       POST: () => new Response("Upload disabled", { status: 200 }),
     };
-  }
-  const { ourFileRouter } = await import("./core");
-  return createRouteHandler({ router: ourFileRouter });
-};
 
-export async function GET(req: Request) {
-  const handlers = await getHandlers();
-  return handlers.GET(req);
-}
+export const { GET, POST } = handlers;
